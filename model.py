d71004fde7cc7697bd3fda5f92bafdf105f0a417
mutia1997 <158068300+mutia1997@users.noreply.github.com>
2024-06-06 15:10:44 +0700

    model.py

--- model.py
+++ model.py
@@ -33,36 +33,29 @@ def predict_price(data, test_size, random_state, method):
 
     model.fit(X_train, y_train)
 
-    # Predict
-    y_pred = model.predict(X_test)
-
-    # Evaluation metrics
-    mae = mean_absolute_error(y_test, y_pred)
-    mape = mean_absolute_percentage_error(y_test, y_pred)
-
-    return mae, mape, model
-
-# Function to calculate Mean Absolute Percentage Error (MAPE)
-def mean_absolute_percentage_error(y_true, y_pred):
-    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+    return model
 
 # Train model for all kecamatan
 models = {}
 for kecamatan, data in zip(["Tanah Abang", "Menteng", "Kemayoran", "Cempaka Putih", "Senen", "Gambir"], [tanah_abang_data, menteng_data, kemayoran_data, cempaka_putih_data, senen_data, gambir_data]):
     if kecamatan == "Tanah Abang":
-        mae, mape, model = predict_price(data, test_size=0.05, random_state=42, method='Grid Search with Cross-Validation')
+        model = predict_price(data, test_size=0.05, random_state=42, method='Grid Search with Cross-Validation')
     elif kecamatan == "Menteng":
-        mae, mape, model = predict_price(data, test_size=0.25, random_state=60, method='Grid Search with Cross-Validation')
+        model = predict_price(data, test_size=0.25, random_state=60, method='Grid Search with Cross-Validation')
     elif kecamatan == "Kemayoran":
-        mae, mape, model = predict_price(data, test_size=0.2, random_state=42, method='Random Forest')
+        model = predict_price(data, test_size=0.2, random_state=42, method='Random Forest')
     elif kecamatan == "Cempaka Putih":
-        mae, mape, model = predict_price(data, test_size=0.1, random_state=42, method='Random Forest')
+        model = predict_price(data, test_size=0.1, random_state=42, method='Random Forest')
     elif kecamatan == "Senen":
-        mae, mape, model = predict_price(data, test_size=0.15, random_state=60, method='Random Forest')
+        model = predict_price(data, test_size=0.15, random_state=60, method='Random Forest')
     elif kecamatan == "Gambir":
-        mae, mape, model = predict_price(data, test_size=0.2, random_state=42, method='Grid Search with Cross-Validation')
+        model = predict_price(data, test_size=0.2, random_state=42, method='Grid Search with Cross-Validation')
     
-    models[kecamatan] = (mae, mape, model)
+    models[kecamatan] = model
+
+# Function to calculate Mean Absolute Percentage Error (MAPE)
+def mean_absolute_percentage_error(y_true, y_pred):
+    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
 
 # Prediksi harga dan MAPE untuk semua kecamatan
 st.title("Prediksi Harga Apartemen di Jakarta Pusat")
@@ -79,10 +72,7 @@ luas_bangunan = st.number_input("Luas Bangunan (m²)", min_value=20, max_value=2
 # Button to show estimated prices
 if st.button('Lihat Estimasi Harga Jual'):
     st.header("Estimasi Harga Apartemen dan MAPE untuk Semua Kecamatan")
-    for kecamatan, (mae, mape, model) in models.items():
+    for kecamatan, model in models.items():
         predicted_price = model.predict([[jumlah_kamar_tidur, jumlah_kamar_mandi, luas_bangunan]])
-        predicted_price_formatted = "{:,}".format(predicted_price[0])
         st.write(f"Kecamatan: {kecamatan}")
-        st.write(f"Estimasi Harga Apartemen: Rp {predicted_price_formatted}")
-        st.write(f"Mean Absolute Percentage Error: {mape:.2f}%")
-        st.write("---")
+        st.write(f"Estimasi Harga Apartemen: Rp {predicted_price[0]:,.0f}")
